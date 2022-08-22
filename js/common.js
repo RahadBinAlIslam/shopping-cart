ab66a1ab9c833360cf00546244459caa51773a47
R4H4D <rahadbinalislam@gmail.com>
2022-08-22 21:57:13 +0600

    finalized

---
+++ js/common.js
@@ -0,0 +1,19 @@
+function setTextElementValueById(elementId, value) {
+  const subTotalElement = document.getElementById(elementId);
+  subTotalElement.innerText = value;
+}
+
+function calculateSubTotal() {
+  const currentPhoneTotal = getTextElementValueById("phone-total");
+  const currentCaseTotal = getTextElementValueById("case-total");
+
+  const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+  setTextElementValueById("sub-total", currentSubTotal);
+
+  const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+  const taxAmount = parseFloat(taxAmountString);
+  setTextElementValueById("tax-amount", taxAmount);
+
+  const finalAmount = currentSubTotal + taxAmount;
+  setTextElementValueById("final-total", finalAmount);
+}
